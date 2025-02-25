ca7ef3744517f0f0e14f7668aa85476672a97d25
as <allensnape@gmail.com>
2025-02-25 17:30:32 +0800

    fix: dev

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,8 +1,9 @@
+import GoCrudVitePlugin from "@allape/gocrud-react/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react(), GoCrudVitePlugin(), viteSingleFile()],
 });
